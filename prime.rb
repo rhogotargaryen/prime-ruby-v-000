70a5449f062596672b6c47811d2612d14f9ba35d
Derek Haller <dhaller.personal@gmail.com>
2018-01-26 03:46:48 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,12 @@
 # Add  code here!
 def prime?(num)
-  if num % 2 = 0
+  if num % 2 === 0
     return false
-  elsif num % 3 = 0
+  elsif num % 3 === 0
     return false
-  elsif num % = 0
+  elsif num % ==== 0
     return false
-  elsif num % 7 = 0
+  elsif num % 7 === 0
     return false
   else
     return true
