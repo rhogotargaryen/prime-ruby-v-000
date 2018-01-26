b122c240ce18d61979fd471deefb9fa1de5a0fab
Derek Haller <dhaller.personal@gmail.com>
2018-01-26 03:44:51 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+def prime?(num)
+  if num % 2 == 0
+    return false
+  elsif num % 3 == 0
+    return false
+  elsif num % == 0
+    return false
+  elsif num % 7 == 0
+    return false
+  else
+    return true
+  end
+end
